a08b298637d648df709341e425ae83948fe4e639
prabhjyot.virdi <prabhjyot.virdi@HHSL9YR8PL2.itsc.hhs-itsc.local>
2018-11-07 21:24:09 -0500

    Target table DDL and grants for BITS interface

---
+++ database/WHRSC_HR_DB_BITS_01_table_permission.sql
@@ -0,0 +1,4 @@
+----------------------------------
+-- Grant for HHS_WHRSC_HR_RW_ROLE
+----------------------------------
+GRANT DELETE, INSERT, SELECT, UPDATE ON HHS_WHRSC_HR.BITS_NEOStatus TO HHS_WHRSC_HR_RW_ROLE;
