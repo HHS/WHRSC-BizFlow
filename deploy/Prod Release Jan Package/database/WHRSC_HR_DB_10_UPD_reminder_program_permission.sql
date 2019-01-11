2d70d702289f63b6e0eec01b53dfd9ddd4d6fb07
Youngjinz <yjoh@bizflow.com>
2019-01-10 19:34:38 -0500

    PROD package

---
+++ deploy/Prod Release Jan Package/database/WHRSC_HR_DB_10_UPD_reminder_program_permission.sql
@@ -0,0 +1,8 @@
+--=============================================================================
+-- Grant privileges on objects under WHRSC schema to roles
+-------------------------------------------------------------------------------
+
+
+-- privilege for HHS_WHRSC_HR_RW_ROLE;
+
+GRANT EXECUTE ON HHS_WHRSC_HR.SP_APPOINTEE_SECURITY_EMAIL TO HHS_WHRSC_HR_RW_ROLE;
